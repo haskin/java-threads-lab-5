1188cdd59630b7ff0d69e1ed512e3bc5fff1adb3
Adrian Khaskin <haskin.dev@gmail.com>
2022-07-26 16:48:11 -0400

    Finished implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        // your code here
+    public static void main(String[] args) throws Exception {
+
+        SavingsAccount savingsAccount = new SavingsAccount();
+        List<Thread> threads = new ArrayList<>();
+
+        long[] deposits = { 1000, 1000, 1000, 1000, 1000 };
+        long[] withdraws = { 100000, -1, 0, 500 };
+        Arrays.stream(deposits).forEach(deposit -> threads.add(new Thread(() -> savingsAccount.deposit(deposit))));
+        Arrays.stream(withdraws).forEach(withdraw -> threads.add(new Thread(() -> savingsAccount.withdraw(withdraw))));
+        for (Thread thread : threads) {
+            thread.start();
+        }
+        for (Thread thread : threads) {
+            thread.join();
+        }
+        System.out.println(savingsAccount.getTotal());
+        return;
     }
 }
--- src/main/java/SavingsAccount.java
+++ src/main/java/SavingsAccount.java
@@ -1,16 +1,51 @@
+import java.util.concurrent.locks.ReentrantLock;
+
 public class SavingsAccount {
 
-  private long total = 0;
+	private final ReentrantLock lock = new ReentrantLock();
 
-  public boolean withdraw(long amount) {
-      
-  }
+	private long total = 0;
 
-  public void deposit(long amount) {
+	public boolean withdraw(long amount) {
+		if (amount > total || amount < 0)
+			return false;
+		while (lock.isLocked()) {
+		}
+		lock.lock();
+		try {
+			total = total - amount;
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		} finally {
+			lock.unlock();
+		}
+	}
 
-  }
+	public void deposit(long amount) {
+		while (lock.isLocked()) {
+		}
+		lock.lock();
+		try {
+			total = total + amount;
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			lock.unlock();
+		}
+	}
 
-  public long getTotal() {
-      return total;
-  }
+	public long getTotal() {
+		return total;
+	}
+
+	public static void main(String[] args) {
+		SavingsAccount sa = new SavingsAccount();
+		sa.deposit(1);
+		System.out.println(sa.withdraw(2));
+		System.out.println(sa.withdraw(-1));
+		System.out.println(sa.withdraw(1));
+
+	}
 }
