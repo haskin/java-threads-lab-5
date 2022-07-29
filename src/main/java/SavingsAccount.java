3655e172299e6153966e52133bdfd43a0667891b
Adrian Khaskin <haskin.dev@gmail.com>
2022-07-28 22:04:31 -0400

    fixed lock implementation

--- src/main/java/SavingsAccount.java
+++ src/main/java/SavingsAccount.java
@@ -1,44 +1,40 @@
+import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class SavingsAccount {
 
-	private final ReentrantLock lock = new ReentrantLock();
+	private final Lock lock = new ReentrantLock();
 
 	private long total = 0;
 
 	public boolean withdraw(long amount) {
-		while (lock.isLocked()) {
-		}
-		lock.lock();
 		try {
+			lock.lock();
 			if (amount > total || amount < 0)
 				return false;
-
 			total = total - amount;
 			return true;
-		} catch (Exception e) {
-			e.printStackTrace();
-			return false;
 		} finally {
 			lock.unlock();
 		}
 	}
 
 	public void deposit(long amount) {
-		while (lock.isLocked()) {
-		}
-		lock.lock();
 		try {
+			lock.lock();
 			total = total + amount;
-		} catch (Exception e) {
-			e.printStackTrace();
 		} finally {
 			lock.unlock();
 		}
 	}
 
 	public long getTotal() {
-		return total;
+		try {
+			lock.lock();
+			return total;
+		} finally {
+			lock.unlock();
+		}
 	}
 
 	public static void main(String[] args) {
